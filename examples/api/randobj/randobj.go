6c3d333c7cc59aadabcd5e4cf996233e4dc5051c
Aaron Boodman <aaron@aaronboodman.com>
2017-07-16 23:08:21 -0700

    Fix some rot in examples/randobj.
    Also add a buildable file, like examples/demo has.

--- examples/api/randobj/randobj.go
+++ examples/api/randobj/randobj.go
@@ -1,11 +1,3 @@
-## Random Ipfs Objects
-During the development, ive frequently found the need to just get a hash of some
-random ipfs object. At first, I would just ask in irc "can someone give me a
-hash?". But I decided I could do better, So I decided to make it a service. In
-this article, im going to go over how I did that (hint: its really simple!)
-
-First, lets get some imports:
-```
 package main
 
 import (
@@ -18,18 +10,9 @@ import (
 	"github.com/ipfs/go-ipfs/repo/fsrepo"
 	"golang.org/x/net/context"
 )
-```
 
-This just pulls in some basic ipfs packages, and the default golang http server.
-Now, since im lazy, im going to have a global for our ipfsnode.
-
-```
 var gnode *core.IpfsNode
-```
 
-Now, lets write the http handler func for generating our random objects.
-
-```
 func ServeIpfsRand(w http.ResponseWriter, r *http.Request) {
 	read := io.LimitReader(u.NewTimeSeededRand(), 2048)
 
@@ -41,31 +24,17 @@ func ServeIpfsRand(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(str))
 	}
 }
-```
 
-And now, lets tie it all together in a main function.
-
-Set up our node configuration, and use the users standard ipfs configuration directory.
-
-```
 func main() {
 	r, err := fsrepo.Open("~/.ipfs")
 	if err != nil {
 		panic(err)
 	}
-```
 
-Now we need to set up our context
-
-```
 	// Make our 'master' context and defer cancelling it
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-```
 
-Then create a configuration and finally create our node!
-
-```
 	cfg := &core.BuildCfg{
 		Repo:   r,
 		Online: true,
@@ -82,6 +51,3 @@ Then create a configuration and finally create our node!
 	http.HandleFunc("/ipfsobject", ServeIpfsRand)
 	http.ListenAndServe(":8080", nil)
 }
-```
-
-By [whyrusleeping](http://github.com/whyrusleeping)
